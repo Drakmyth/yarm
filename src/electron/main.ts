143915a044660e93577e05503b0d220c31afe60c
Drakmyth <shaun.hamman@gmail.com>
2022-06-09 19:39:29 -0700

    Remove React CDN references

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,8 @@ import { app, BrowserWindow } from "electron";
 const openWindow = (): void => {
   const window = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    title: `Yet Another Rom Manager`,
   });
 
   window.setMenuBarVisibility(false);
