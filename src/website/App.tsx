72790c765a54169b314bcdc2c33981a1802eb9d1
Drakmyth <shaun.hamman@gmail.com>
2022-06-12 11:37:58 -0700

    Add window controls and title, update titlebar styling

--- src/website/App.tsx
+++ src/website/App.tsx
@@ -13,37 +13,53 @@ const app = document.getElementById("app");
 
 const Titlebar = () => (
     <>
-        <Menu menuButton={<MenuButton>File</MenuButton>}>
-            <AcceleratedMenuItem label="New..." ctrl hotkey="N" />
-            <AcceleratedMenuItem label="Open..." ctrl hotkey="O" />
-            <SubMenu label="Open Recent"></SubMenu>
-            <AcceleratedMenuItem label="Close" ctrl hotkey="W" />
-            <MenuDivider />
-            <AcceleratedMenuItem label="Save" ctrl hotkey="S" />
-            <AcceleratedMenuItem label="Save As..." ctrl shift hotkey="S" />
-            <MenuDivider />
-            <MenuItem>Preferences...</MenuItem>
-            <MenuDivider />
-            <AcceleratedMenuItem label="Exit" ctrl hotkey="Q" />
-        </Menu>
-        <Menu menuButton={<MenuButton>View</MenuButton>}>
-            <MenuItem>Language</MenuItem>
-            <MenuDivider />
-            <MenuItem type="checkbox" checked={true}>Show Toolbar</MenuItem>
-            <MenuDivider />
-            <MenuItem>Configure Columns...</MenuItem>
-        </Menu>
-        <Menu menuButton={<MenuButton>Help</MenuButton>}>
-            <MenuItem>Check for Updates...</MenuItem>
-            <MenuItem>Release Notes</MenuItem>
-            <MenuDivider />
-            <MenuItem>Visit GitHub</MenuItem>
-            <MenuItem>Report Issue</MenuItem>
-            <MenuDivider />
-            <AcceleratedMenuItem label="Show Dev Tools" ctrl shift hotkey="I" />
-            <MenuDivider />
-            <MenuItem>About</MenuItem>
-        </Menu>
+        <div className="titlebar-menus">
+            <Menu menuButton={<MenuButton>File</MenuButton>}>
+                <AcceleratedMenuItem label="New..." ctrl hotkey="N" />
+                <AcceleratedMenuItem label="Open..." ctrl hotkey="O" />
+                <SubMenu label="Open Recent"></SubMenu>
+                <AcceleratedMenuItem label="Close" ctrl hotkey="W" />
+                <MenuDivider />
+                <AcceleratedMenuItem label="Save" ctrl hotkey="S" />
+                <AcceleratedMenuItem label="Save As..." ctrl shift hotkey="S" />
+                <MenuDivider />
+                <MenuItem>Preferences...</MenuItem>
+                <MenuDivider />
+                <AcceleratedMenuItem label="Exit" ctrl hotkey="Q" />
+            </Menu>
+            <Menu menuButton={<MenuButton>View</MenuButton>}>
+                <MenuItem>Language</MenuItem>
+                <MenuDivider />
+                <MenuItem type="checkbox" checked={true}>
+                    Show Toolbar
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem>Configure Columns...</MenuItem>
+            </Menu>
+            <Menu menuButton={<MenuButton>Help</MenuButton>}>
+                <MenuItem>Check for Updates...</MenuItem>
+                <MenuItem>Release Notes</MenuItem>
+                <MenuDivider />
+                <MenuItem>Visit GitHub</MenuItem>
+                <MenuItem>Report Issue</MenuItem>
+                <MenuDivider />
+                <AcceleratedMenuItem label="Show Dev Tools" ctrl shift hotkey="I" />
+                <MenuDivider />
+                <MenuItem>About</MenuItem>
+            </Menu>
+        </div>
+        <div className="titlebar-title">Yarm</div>
+        <div className="titlebar-controls">
+            <MenuButton className="window-control">
+                <i className="fa-solid fa-minus"></i>
+            </MenuButton>
+            <MenuButton className="window-control">
+                <i className="fa-regular fa-square"></i>
+            </MenuButton>
+            <MenuButton className="window-control close-button">
+                <i className="fa-solid fa-xmark"></i>
+            </MenuButton>
+        </div>
     </>
 );
 
