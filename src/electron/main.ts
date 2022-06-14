956d009adc849e1b377a9952fb677007ea341159
Drakmyth <shaun.hamman@gmail.com>
2022-06-14 13:14:13 -0700

    Add menu accelerator hotkey support

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -4,6 +4,62 @@ import path from "path";
 // Only disable security warning in development
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = `${process.env.NODE_ENV !== "production"}`;
 
+interface HotKeyDefinition {
+    ctrl: boolean;
+    shift: boolean;
+    alt: boolean;
+    handler: (win: BrowserWindow) => void
+}
+
+const hotkeys: Record<string, HotKeyDefinition[]> = {
+    "N": [
+        {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            handler: (win: BrowserWindow) => console.log("New...")
+        }
+    ],
+    "O": [
+        {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            handler: (win: BrowserWindow) => console.log("Open...")
+        }
+    ],
+    "W": [
+        {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            handler: (win: BrowserWindow) => console.log("Close")
+        }
+    ],
+    "S": [
+        {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            handler: (win: BrowserWindow) => console.log("Save")
+        },
+        {
+            ctrl: true,
+            shift: true,
+            alt: false,
+            handler: (win: BrowserWindow) => console.log("Save As...")
+        }
+    ],
+    "Q": [
+        {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            handler: (win: BrowserWindow) => win.close()
+        }
+    ]
+};
+
 const openWindow = (): void => {
     const window = new BrowserWindow({
         width: 800,
@@ -19,9 +75,23 @@ const openWindow = (): void => {
 
     window.loadFile(path.join(__dirname, "index.html"));
 
+    window.webContents.on("before-input-event", (event, input) => {
+        const hkDefs = hotkeys[input.key.toUpperCase()] || [];
+        for (let hk of hkDefs) {
+            if (
+                hk.ctrl === input.control &&
+                hk.shift === input.shift &&
+                hk.alt === input.alt
+            ) {
+                hk.handler(window);
+                event.preventDefault();
+            }
+        }
+    });
+
     ipcMain.on("exit", () => {
         window.close();
-    })
+    });
 
     window.webContents.openDevTools();
 };
